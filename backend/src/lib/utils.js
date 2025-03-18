6f9795c3811dae47cf0f577c457d2aad29cd0671
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-03-18 15:22:26 +0800

    token fixed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,9 +8,10 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development"
+    sameSite: "none",  // For cross-site requests
+    secure: true,  // HTTPS only in production
   });
+  
 
   return token;
 }
