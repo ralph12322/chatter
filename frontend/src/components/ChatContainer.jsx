d0eb49ac7d716fe5297ebc64a4a87c449d96c56d
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-03-18 15:39:25 +0800

    removed the avatar.png

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -59,8 +59,8 @@ const ChatContainer = () => {
                 <img
                   src={
                     message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
+                      ? authUser.profilePic
+                      : selectedUser.profilePic
                   }
                   alt="profile pic"
                 />
