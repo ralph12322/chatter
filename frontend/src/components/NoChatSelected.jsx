3bfe1d258d42eb678646be800b60d29d96a3f892
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-03-18 17:31:45 +0800

    r

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -9,7 +9,7 @@ const NoChatSelected = () => {
           <div className="relative">
             <div
               className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
-             justify-center animate-wiggle"
+             justify-center animate-bounce"
             >
               <MessageSquare className="w-8 h-8 text-primary " />
             </div>
